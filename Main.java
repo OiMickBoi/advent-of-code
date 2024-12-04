2a8a878f1bd67075f4b6a86e8d3a091a9af709b2
oimickboi <oimickboi@gmail.com>
2024-12-03 17:36:42 -0700

    test increasing functions

--- Main.java
+++ Main.java
@@ -4,9 +4,7 @@ import java.io.IOException;
 import java.util.*;
 
 class Main {
-    // static ArrayList<Integer> list = new ArrayList<>();
     static ArrayList<ArrayList<Integer>> list = new ArrayList<ArrayList<Integer>>(); 
-    static HashMap<Integer,Integer> map = new HashMap<>();
   
     public static void init() {
         try (BufferedReader br = new BufferedReader(new FileReader("input.txt"))) {
@@ -25,23 +23,38 @@ class Main {
         }
     }
 
-    public static int eval() {
-        int safeReports = 0;
-        for (ArrayList<Integer> l : list) {
-            int t = 0;
-            int len = l.size();
-            for (int i = 0; i < len-1; i++) {
-                int x = l.get(i);
-                int y = l.get(i+1);
-                if (cal(x, y)) t++;
+    // public static int eval() {
+    //     int safeReports = 0;
+    //     for (ArrayList<Integer> line : list) {
+    //         int t = 0;
+    //         int len = line.size();
+
+    //         isSafe(line);
+    //         if (t == len-1) safeReports++;
+    //     }
+    //     return safeReports;
+    // }
+
+    // public static boolean isSafe(ArrayList<Integer> line) {
+    //     for (int i = 0; i < len-1; i++) {
+    //         int x = line.get(i);
+    //         int y = line.get(i+1);
+    //         if (isSafe(x, y)) t++;
+    //     }
+    // }
+
+    
+    public static boolean isIncreasing(int x, int y) {
+        System.out.println(y + " - " + x);
+            if (y-x > 0 && y-x < 4) {
+                return true;
             }
-            if (t == len) safeReports++;
-        }
-        return safeReports;
+        return false;
     }
     
-    public static boolean cal(int x, int y) {
-            if (Math.abs(x-y) > 0 && Math.abs(x-y) < 3) {
+    public static boolean isDecreasing(int x, int y) {
+        System.out.println(x + " - " + y);
+            if (x-y > 0 && x-y < 4) {
                 return true;
             }
         return false;
@@ -50,52 +63,22 @@ class Main {
     public static void main(String[] args) {
         init();
 
-        // for (int i = 0; i < list.size(); i++) {
-        //     System.out.println(list.get(i));
-        // }
-        System.out.println(eval());
+        System.out.println(isIncreasing(1,2));
+        System.out.println(isIncreasing(1,3));
+        System.out.println(isIncreasing(1,4));
+        System.out.println(isIncreasing(1,5)); // too much
+        System.out.println(isIncreasing(1,0)); // decreasing
+
+        System.out.println(isDecreasing(0,1));
+        System.out.println(isDecreasing(1,0));
+        System.out.println(isDecreasing(2,1));
+        System.out.println(isDecreasing(3,1));
+        System.out.println(isDecreasing(4,1));
+        System.out.println(isDecreasing(5,1)); // toomuch
+        
+        // System.out.println(eval());
     }
 }
 
-// class Main {
-//     static ArrayList<Integer> list = new ArrayList<>();
-//     static HashMap<Integer,Integer> map = new HashMap<>();
-  
-//     public static void init() {
-//         try (BufferedReader br = new BufferedReader(new FileReader("input.txt"))) {
-//             String line;
-//             while ((line = br.readLine()) != null) {
-//                 String[] words = line.split("\\s+");
-//                 list.add(Integer.parseInt(words[0]));
-//                 int key = Integer.parseInt(words[1]);
-
-//                 key = Integer.parseInt(words[1]);
-//                 map.put(key, map.getOrDefault(key, 0) + 1);
-//             }
-//             br.close();
-//         } catch (IOException e) {
-//             System.err.println("Error reading file: " + e.getMessage());
-//         }
-//     }
-
-    
-//     public static int cal() {
-//         Collections.sort(list);
-//         int sum = 0;
-//         for (int i = 0; i < list.size(); i++) {
-//             Integer key = list.get(i);
-//             // Use getOrDefault to handle null values from map
-//             if (key != null) {
-//                 sum += key * map.getOrDefault(key, 0);
-//             }
-//         }
-//         return sum;
-//     }
-
-//     public static void main(String[] args) {
-//         init();
-//         System.out.println(cal());
-//     }
-// }
  
 
